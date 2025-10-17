b9ee07985b5bff47bb60d12a9ceb6ebcb8285348
Mayokun <mayokunlaoshelawal@gmail.com>
2025-10-17 06:28:52 +0100

    final touches

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="w-full bg-black py-2 mt-auto text-white flex items-center justify-end">
-        <img src="{{url('images/logo/logo-white.png')}}" alt="" srcset="" class="h-[20px]">
-        <span class="fa text-white text-[15px] w-auto mx-[5px]">CJLUXURY</span>
-        <span class="text-white text-[15px] w-auto mx-[5px]" style="font-family: MTNBrighterSans-Regular"> | &copy; {{date('Y')}}</span>
+        <img src="{{url('images/logo/logo-white.png')}}" alt="" srcset="" class="h-[15px]">
+        <span class="fa text-white text-[10px] w-auto mx-[5px]">CJLUXURY</span>
+        <span class="text-white text-[10px] w-auto mx-[5px]" style="font-family: MTNBrighterSans-Regular">&copy; {{date('Y')}}</span>
 </footer>
